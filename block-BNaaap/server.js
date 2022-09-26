135fdefbc0345e27f3a4650ae22a157b29a73de0
ravi11o <ravi.suraj110@gmail.com>
2022-09-26 13:30:16 +0530

    status code

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,10 +3,11 @@ var http=require('http');
 var server=http.createServer(handleRequest);
 
 function handleRequest(req,res){
-  res.writeHead('201',{'content-type':'text/html'});
-  res.write('Hello');
-  res.write('world');
-  res.end();
+  // res.writeHead('201',{'content-type':'text/html'});
+  console.log(req.method)
+  res.statusCode=201;
+  res.setHeader('Content-Type','text/html')
+  res.end('<h1>welcome to hp <h1>');
 }
 server.listen(4444,()=>{
   console.log('we are listing on port 4444')
