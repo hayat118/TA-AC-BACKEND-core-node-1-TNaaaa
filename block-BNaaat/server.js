bb7c8cd4893f5e33c646fcd15408b7553fc0e4f2
ravi11o <ravi.suraj110@gmail.com>
2022-09-27 17:07:04 +0530

    read and stream file

--- block-BNaaat/server.js
+++ block-BNaaat/server.js
@@ -4,16 +4,28 @@ var fs=require('fs');
 
 var server=http.createServer(handleRequest);
 
+// function handleRequest(req,res){
+// if(req.method==='GET',req.url==='/'){
+//   res.end('Welcome to index page')
+// }else if(req.method==='GET',req.url==='/file'){
+//   res.setHeader('Content-type','text/html')
+//   fs.readFile('./node.html')
+// }else if (req.method==='GET',req.url==='/file'){
+//   res.setHeader('Content-type','text/stream')
+//   fs.createReadStrean('./node.html').pipe(res)
+// }
+// }
 function handleRequest(req,res){
-if(req.method==='GET',req.url==='/'){
-  res.end('Welcome to index page')
-}else if(req.method==='GET',req.url==='/file'){
-  res.setHeader('Content-type','text/html')
-  fs.readFile('./node.html')
-}else if (req.method==='GET',req.url==='/file'){
-  res.setHeader('Content-type','text/stream')
-  fs.createReadStrean('./node.html').pipe(res)
-}
+  if(req.method==='GET' && req.url==='/file'){
+    fs.readFile('./node.html',(err,content)=>{
+      if(err) console.log(err);
+      res.setHeader('content-type','text/html');
+      res.end(content);
+    })
+  }if(req.method==='GET',req.url==='/stream'){
+      res.setHeader('content-type','text/html');
+      fs.createReadStream('./node.html').pipe(res)
+  }
 }
 
 server.listen(5555,()=>{
